16f0436237d9ff673b5f15a423940ab7793641c2
Roxedus <me@roxedus.dev>
2023-03-16 19:08:40 +0100

    Logic vanished in push

--- custom-cont-init.d/install-cert.sh
+++ custom-cont-init.d/install-cert.sh
@@ -1,32 +1,99 @@
-#!/bin/bash
+#!/usr/bin/with-contenv bash
+
+distro=""
+arch=""
+
+if [[ -f /usr/bin/apt ]]; then
+    distro="debian"
+elif [[ -f /sbin/apk ]]; then
+    distro="alpine"
+elif [[ -f /usr/bin/dnf ]]; then
+    distro="fedora"
+elif [[ -f /usr/sbin/pacman ]]; then
+    distro="arch"
+fi
+
+case "$distro" in
+"debian")
+    arch=$(dpkg --print-architecture)
+    ;;
+"alpine")
+    arch=$(uname -m)
+    ;;
+"fedora")
+    echo "Fedora is currently not supported."
+    exit 1
+    ;;
+"arch")
+    echo "ArchLinux is currently not supported."
+    exit 1
+    ;;
+esac
 
 until [ -f /cert/mitmproxy-ca-cert.cer ]; do
     sleep 2
     echo "no cert yet"
 done
-if [ ! -f "/usr/share/ca-certificates/mitm/mitmproxy-ca.cer" ]; then
-    mkdir -p /usr/share/ca-certificates/mitm
-    cp /cert/mitmproxy-ca-cert.cer /usr/share/ca-certificates/mitm/mitmproxy-ca.cer
-    chmod 644 /usr/share/ca-certificates/mitm/mitmproxy-ca.cer
-    echo "mitm/mitmproxy-ca.cer" >>/etc/ca-certificates.conf
-    update-ca-certificates
+if [ ! -d "/mitm" ]; then
 
-    javaversions="java-8-openjdk-amd64"
+    mkdir -p /mitm
+    cp /cert/mitmproxy* /mitm
+    chmod +rx /mitm/mitmproxy*
+
+    echo -n "/mitm/mitmproxy-ca.pem" >/run/s6/container_environment/CURL_CA_BUNDLE
+
+    if [ $distro == "debian" ]; then
+        mkdir -p /usr/share/ca-certificates/mitm
+        cp /mitm/mitmproxy-ca-cert.cer /usr/share/ca-certificates/mitm/mitmproxy-ca.cer
+        chmod 644 /usr/share/ca-certificates/mitm/mitmproxy-ca.cer
+        echo "mitm/mitmproxy-ca.cer" >>/etc/ca-certificates.conf
+        update-ca-certificates
+        echo "Updated debian store"
+    fi
+
+    if [ $distro == "alpine" ]; then
+        cat /mitm/mitmproxy-ca-cert.cer >>/etc/ssl/certs/ca-certificates.crt
+        echo "Updated alpine store"
+    fi
+
+    if [ -x "$(command -v python3)" ]; then #https://incognitjoe.github.io/adding-certs-to-requests.html
+        echo -n "/mitm/mitmproxy-ca.pem" >/run/s6/container_environment/REQUESTS_CA_BUNDLE
+        echo -n "/mitm/mitmproxy-ca.pem" >/run/s6/container_environment/SSL_CERT_FILE
+        python3 -c '
+try:
+    import certifi
+    cafile = certifi.where()
+    with open("/mitm/mitmproxy-ca.pem", "rb") as infile:
+        customca = infile.read()
+    with open(cafile, "ab") as outfile:
+        outfile.write(customca)
+    print("Installed certifi cert")
+except ImportError:
+    pass
+'
+    fi
+
+    javaversions="/usr/lib/jvm/java-8-openjdk-$arch/jre/lib/security /usr/lib/jvm/java-11-openjdk-$arch/jre/lib/security /usr/lib/jvm/java-11-openjdk-$arch/lib/security /usr/lib/jvm/java-17-openjdk-$arch/lib/security /config/data/keystore"
 
     for javaver in $javaversions; do
-        if [ -f "/usr/lib/jvm/$javaver/jre/lib/security/cacerts" ]; then
-            keytool -importcert -trustcacerts -keystore /usr/lib/jvm/$javaver/jre/lib/security/cacerts \
-                -storepass changeit -noprompt -alias mmitm -file /cert/mitmproxy-ca-cert.pem
+        if [ -f "$javaver/cacerts" ]; then
+            keytool -importcert -trustcacerts -keystore "$javaver/cacerts" \
+                -storepass changeit -noprompt -alias mmitm -file /mitm/mitmproxy-ca-cert.pem
             echo "Added cert to $javaver keystore"
         fi
     done
 
-    apt update
-    apt install iptables net-tools iproute2 -y --no-install-recommends
+    if [ -x "$(command -v php)" ]; then
+        sed -i 's#;curl.cainfo=#curl.cainfo=/mitm/mitmproxy-ca-cert.cer#' /etc/php*/php.ini
+        sed -i 's#;openssl.cafile=#openssl.cafile=/mitm/mitmproxy-ca-cert.cer#' /etc/php*/php.ini
+        echo "Updated php store"
+    fi
 
-    iptables -t nat -A OUTPUT -p tcp -m owner ! --uid-owner 0 --dport 443 -j REDIRECT --to-port 8080
-    iptables -t nat -A OUTPUT -p tcp -m owner ! --uid-owner 0 --dport 80 -j REDIRECT --to-port 8080
 fi
 
-s6-setuidgid abc curl -I -s -w "%{http_code}\n" -o /dev/null -x http://localhost:8080 https://www.google.no
-s6-setuidgid abc curl -I -s -w "%{http_code}\n" -o /dev/null https://www.vg.no
+while ! nc -z localhost 42069; do
+    sleep 0.5
+done
+
+s6-setuidgid abc curl -I -s -w "%{http_code}\n" -o /dev/null -x http://localhost:42069 https://www.google.com || echo "Curl to mitm failed"
+s6-setuidgid abc curl -I -s -w "%{http_code}\n" -o /dev/null https://www.linuxserver.io
